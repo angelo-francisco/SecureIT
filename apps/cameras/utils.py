ab2c8c5e3590ad1601014d1edbaac32f331d26ef
angelo-francisco <ics20080729@gmail.com>
2026-01-11 19:09:07 +0100

    removendo comentarios desnecessarios

--- apps/cameras/utils.py
+++ apps/cameras/utils.py
@@ -2,13 +2,9 @@ import cv2
 
 
 def get_cameras():
-    """
-    Lista câmeras disponíveis de forma compatível
-    com Windows, Linux e macOS usando OpenCV puro.
-    """
     cameras = []
 
-    for index in range(5):  # tenta de 0 a 4
+    for index in range(5):
         cap = cv2.VideoCapture(index)
         if cap.isOpened():
             cameras.append({
