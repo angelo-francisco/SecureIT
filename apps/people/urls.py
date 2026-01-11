eebf0ed1814d88ba6e68fc64990aba3b47e27ee0
angelo-francisco <ics20080729@gmail.com>
2026-01-11 19:57:01 +0100

    configurando a view adicionar pessoa

--- apps/people/urls.py
+++ apps/people/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from .views import (
-    home
-)
+from .views import home, new_person
 
 app_name = "people"
 
-urlpatterns = [path("", home, name="home")]
+urlpatterns = [path("", home, name="home"), path("new/", new_person, name="new")]
--- apps/people/views.py
+++ apps/people/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "people/home.html")
+
+
+def new_person(request):
+    return render(request, "people/new.html")
