4324096a015b39b78759dfbcc837872bddf609df
angelo-francisco <ics20080729@gmail.com>
2026-01-13 16:24:20 +0100

    adiciona os dados das homes, hosts ao context

--- apps/people/views.py
+++ apps/people/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from .models import Home, Resident, FIELDS
+
 
 def home(request):
     return render(request, "people/home.html")
 
 
 def new_person(request):
-    return render(request, "people/new.html")
+    homes = Home.objects.all()
+    hosts = Resident.objects.select_related("person", "home").all()
+    return render(request, "people/new.html", {"homes": homes, "hosts": hosts, "fields": FIELDS})
