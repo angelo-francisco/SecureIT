8eef215012342e63cac7fdc91e6cdf063bc8e232
angelo-francisco <ics20080729@gmail.com>
2026-01-14 13:00:29 +0100

    removendo views sem url correspondente

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,26 +1,11 @@
 from django.urls import path
-from .views import (
-    signup,
-    login_page,
-    signup_api,
-    login_api,
-    dashboard,
-    logout_view
-)
+from .views import signup, login, logout
 
 app_name = "users"
 
 urlpatterns = [
-    # páginas
     path("signup/", signup, name="signup"),
-    path("login/", login_page, name="login"),
-
-    # API
-    path("api/signup/", signup_api),
-    path("api/login/", login_api),
-
-    # sessão
-    path("dashboard/", dashboard),
-    path("logout/", logout_view),
+    path("login/", login, name="login"),
+    path("logout/", logout, name="logout"),
 ]
 
