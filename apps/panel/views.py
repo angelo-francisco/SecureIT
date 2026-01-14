5f8c429531ba8bc03d6bb084bd27edff3a894d04
angelo-francisco <ics20080729@gmail.com>
2026-01-14 14:41:07 +0100

    adicionando a plataforma

--- apps/panel/views.py
+++ apps/panel/views.py
@@ -1,7 +1,12 @@
+from platform import system
+
 from django.shortcuts import render
+
 from apps.cameras.models import Camera
 
 
 def panel(request):
     cameras = Camera.objects.all()
-    return render(request, "panel/panel.html", {"cameras": cameras})
+    return render(
+        request, "panel/panel.html", {"cameras": cameras, "platform": system()}
+    )
