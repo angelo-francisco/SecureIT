e117924e83639247cfe74e21bd3e02eafc0c3b7c
angelo-francisco <ics20080729@gmail.com>
2026-01-14 09:44:20 +0100

    model user agora mais precisa

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,51 +1,19 @@
 from django.db import models
 
-from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.hashers import make_password, check_password
 
 
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-
-    def create_user(self, phone, pin=None, **extra_fields):
-        if not phone:
-            raise ValueError('O telefone é obrigatório')
-
-        user = self.model(phone=phone, **extra_fields)
-
-        if pin:
-            user.set_pin(pin)
-
-        # Desativa password tradicional do Django
-        user.set_password(None)
-
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, phone, pin=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        return self.create_user(phone, pin, **extra_fields)
-
-
 class User(AbstractUser):
-    username = None  # Remove username padrão
-    phone = models.CharField(max_length=20, unique=True)
-    pin = models.CharField(max_length=128)
+    username = None
+    email = models.EmailField(unique=True)
+    pin = models.CharField(max_length=4)
 
-    USERNAME_FIELD = 'phone'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
-
-    objects = UserManager()
 
     def set_pin(self, raw_pin):
         self.pin = make_password(raw_pin)
 
     def check_pin(self, raw_pin):
         return check_password(raw_pin, self.pin)
-
-    def __str__(self):
-        return self.phone
