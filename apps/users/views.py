c46d27fdce84de35a800bb7d33ed74b8a518b3cc
angelo-francisco <ics20080729@gmail.com>
2026-01-14 13:00:13 +0100

    criando views abstratas

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,14 +1,26 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth import login as dj_login
-from django.contrib.auth import logout as dj_logout
-from django.contrib.auth.decorators import login_not_required, login_required
+from django.contrib.auth import login as django_login
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_not_required
 from django.shortcuts import redirect, render
 
 User = get_user_model()
 
+
+@login_not_required
 def signup(request):
     return render(request, "users/signup.html")
 
 
-def login_page(request):
+@login_not_required
+def login(request):
     return render(request, "users/login.html")
+
+
+@login_not_required
+def pin(request):
+    return render(request, "users/pin.html")
+
+
+def logout(request):
+    return redirect("")
