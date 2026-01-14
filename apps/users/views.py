d579807ea04f5bc0299f1b79731a318ec90f44a3
angelo-francisco <ics20080729@gmail.com>
2026-01-14 12:56:02 +0100

    refactorando o codigo

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login as dj_login, logout as dj_logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.decorators import login_not_required
 from django.contrib.auth import get_user_model
+from django.contrib.auth import login as dj_login
+from django.contrib.auth import logout as dj_logout
+from django.contrib.auth.decorators import login_not_required, login_required
+from django.shortcuts import redirect, render
 
 User = get_user_model()
 
