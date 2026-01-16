1a83ce77dd67b656beb6ea57a22c94e1a69a25b0
angelo-francisco <ics20080729@gmail.com>
2026-01-16 16:30:50 +0100

    filtrando as cameras por usuario

--- apps/panel/views.py
+++ apps/panel/views.py
@@ -6,7 +6,7 @@ from apps.cameras.models import Camera
 
 
 def panel(request):
-    cameras = Camera.objects.all()
+    cameras = Camera.objects.filter(user=request.user).all()
     return render(
         request, "panel/panel.html", {"cameras": cameras, "platform": system()}
     )
