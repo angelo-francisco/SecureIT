0e802c5a171bb98867eed4d377ff2a7c9a81a0aa
angelo-francisco <ics20080729@gmail.com>
2026-01-19 23:25:23 +0100

    fixing imports

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,6 @@
-from django.db import models
+from django.contrib.auth.hashers import check_password, make_password
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.hashers import make_password, check_password
+from django.db import models
 
 
 class User(AbstractUser):
