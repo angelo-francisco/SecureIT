619898a3936eb1d6f272fbf305db1908f3bb356d
angelo-francisco <ics20080729@gmail.com>
2026-01-29 21:35:24 +0100

    adicionando url dos detalhes ao people.urls

--- apps/people/urls.py
+++ apps/people/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import home, new_person
+from .views import home, new_person, get_person
 
 app_name = "people"
 
 urlpatterns = [
     path("", home, name="home"),
     path("new/", new_person, name="new"),
+    path("<int:id>/", get_person, name="details")
 ]
