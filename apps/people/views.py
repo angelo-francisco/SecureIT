c8c00ee6d26a46b87fc134386a47c41d39971ab4
angelo-francisco <ics20080729@gmail.com>
2026-02-06 05:28:44 +0100

    removing labels for better estetic

--- apps/people/views.py
+++ apps/people/views.py
@@ -78,5 +78,5 @@ def new_person(request):
 def get_person(request, person_id):
     person = get_object_or_404(Person, id=person_id)
     context = {"person": person}
-
+    print(dir(person))
     return render(request, "people/details.html", context)
