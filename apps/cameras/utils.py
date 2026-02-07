16ad1ee537d4f32b657b6aca237662d221b9d68d
angelo-francisco <ics20080729@gmail.com>
2026-02-07 23:12:14 +0100

    ignoring typing;new func: validating con types

--- apps/cameras/utils.py
+++ apps/cameras/utils.py
@@ -4,14 +4,14 @@ from django.core.exceptions import ValidationError
 from .models import Camera, LocalCamera, WifiCamera
 
 
-def create_camera(user, location, connection_type):
-    if not location or not connection_type:
+def create_camera(user, name, location, connection_type):
+    if not location or not connection_type or not name:
         raise ValidationError("Preencha todos os campos, por favor.")
     if connection_type not in ["L", "W"]:
         raise ValidationError("Só aceitamos câmaras locais ou Wi-Fi")
 
     camera = Camera.objects.create(
-        user=user, location=location, connection_type=connection_type
+        user=user, name=name, location=location, connection_type=connection_type
     )
 
     return camera
@@ -20,7 +20,7 @@ def create_camera(user, location, connection_type):
 def create_wifi_camera(camera_id, stream_url, username, password):
     if not stream_url:
         raise ValidationError("Informe a url de video da câmara")
-    return WifiCamera.objects.create(
+    return WifiCamera.objects.create(  # type: ignore
         camera=camera_id,
         stream_url=stream_url,
         username=username,
@@ -41,7 +41,7 @@ def create_local_camera(camera_id, camera_path, cameras_list):
     if not camera_info:
         raise ValidationError("Dados da câmara indisponíveis.")
 
-    return LocalCamera.objects.create(camera_id=camera_id, info=camera_info)
+    return LocalCamera.objects.create(camera_id=camera_id, info=camera_info)  # type: ignore
 
 
 def get_cameras():
@@ -60,3 +60,12 @@ def get_cameras():
             )
             registered.add(cam.path)
     return cams, registered
+
+
+def valid_connection_type(connection_type):
+    if not connection_type:
+        raise ValidationError("Informe o tipo de conexão")
+
+    if connection_type not in ["L", "W"]:
+        raise ValidationError("Tipo de conexão desconhecido")
+    return connection_type
