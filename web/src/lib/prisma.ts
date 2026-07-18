23963c797f56ed10da0ae02dd7a462d0a88f48ae
kiluzx <kiluzxdev@gmail.com>
2026-07-19 00:57:32 +0100

    use http based reqyuest for db

--- web/src/lib/prisma.ts
+++ web/src/lib/prisma.ts
@@ -1,15 +1,17 @@
+import { Pool, neonConfig } from '@neondatabase/serverless';
+
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@/generated/prisma/client";
 
-const connectionString = process.env.DATABASE_URL!;
+neonConfig.poolQueryViaFetch = true;
 
 const globalForPrisma = globalThis as {
   prisma?: PrismaClient;
 };
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaNeon(pool);
 
-const adapter = new PrismaNeon({ connectionString, maxUses: 1 });
-
-export const prisma = 
+export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     adapter,
