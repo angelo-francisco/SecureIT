fc7e28207adee4486b0035cc2c9e3f9b61c73784
kiluzx <kiluzxdev@gmail.com>
2026-07-21 20:13:03 +0100

    fix: use PrismaLibSQL for local SQLite dev, PrismaD1 for Cloudflare

--- web/src/lib/prisma.ts
+++ web/src/lib/prisma.ts
@@ -1,16 +1,27 @@
-import { getCloudflareContext } from "@opennextjs/cloudflare";
-import { PrismaD1 } from "@prisma/adapter-d1";
 import { PrismaClient } from "@/generated/prisma/client";
 
 let _client: PrismaClient | undefined;
 
 function getClient(): PrismaClient {
-  if (!_client) {
+  if (_client) return _client;
+
+  const dbUrl = process.env.DATABASE_URL_SQLITE;
+
+  if (dbUrl?.startsWith("file:")) {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { PrismaLibSQL } = require("@prisma/adapter-libsql");
+    const adapter = new PrismaLibSQL({ url: dbUrl });
+    _client = new PrismaClient({ adapter });
+  } else {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { getCloudflareContext } = require("@opennextjs/cloudflare");
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { PrismaD1 } = require("@prisma/adapter-d1");
     const { env } = getCloudflareContext();
-    // @ts-expect-error DATABASE_URL_SQLITE D1 binding from wrangler
     const adapter = new PrismaD1(env.DATABASE_URL_SQLITE);
     _client = new PrismaClient({ adapter });
   }
+
   return _client;
 }
 
